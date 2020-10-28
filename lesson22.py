41db4f94c6913fb2af19417eb5bc9abf44dadb70
a3fgroup <g.koreev@a3f.yaconnect.com>
2020-10-28 21:28:08 +0300

    add lesson 22

--- lesson22.py
+++ lesson22.py
@@ -1,36 +1,51 @@
-# words = ['мадам', 'топот', 'test', 'madam', 'word']
-
-
-# Первый вариант решения:
-# palindromes = []
-# for word in words:
-#     if word == word[::-1]:
-#         palindromes.append(word)
-
-
-# Второй вариант решения:
-# palindromes = [word for word in words if word == word[::-1]]
-# print(palindromes)
-
-
-
-# my_str = ['Око за око', 'А роза упала на лапу азора', 'Около Миши молоко']
-# palindromes = []
-# for word in my_str:
-#     word_r = word.replace(' ', '').lower()
-#     if word_r == word_r[::-1]:
-#         palindromes.append(word)
+# s = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+# s2 = set('hello')
+# s3 = {i for i in range(1, 11)}
+# s4 = set()
 #
-# print(palindromes)
+#
+# print(s)
+# print(s2)
+# print(s3)
+# print(type(s4))
+#
+#
+# nums = [1, 2, 3, 3, 1, 2, 4, 5]
+# nums2 = list(set(nums))
+# print(nums2)
+
+#
+# a = set('abracadabra')
+# b = set('alacazam')
+#
+# c = a - b   # Вычитание - убираем из а все буквы, которые есть в b
+# d = a | b   # Объединение - буквы в а или в b
+# e = a & b   # Пересечение - буквы и в а и в b
+# f = a ^ b   # Множество из элементов - буквы в а или b, но не в обоих
+#
+# print(a, b, c, d, e, f, sep='\n')
 
 
-l = list(range(1, 10))
-l2 = list('hello')
+# s = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+# s2 = s.copy()
+#
+# print(s, id(s))
+# print(s, id(s2))
+# s.add('melon')
+# print(s)
+# s.remove('orange')
+# print(s)
+#
+# s.discard('orange')
+# print(s)
+#
+# s.pop()
+# print(s)
 
-print(l)
+# s.clear()
+# print(s)
 
-s = '-'.join(map(str, l))
-s2 = ','.join(l2)
 
-print(s)
-print(s2)
+a = frozenset('hello')
+a.add('apple')
+print(a)
