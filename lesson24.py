6569d1c6700524cb0738116b1c83ec691bb8a115
a3fgroup <g.koreev@a3f.yaconnect.com>
2020-11-07 19:05:17 +0300

    add lesson 4

---
+++ lesson24.py
@@ -0,0 +1,17 @@
+product1 = {'title': 'Sony', 'price': 100}
+# print(product1.items())
+# print(product1.keys())
+# print(product1.values())
+# print(product1.pop('title'))
+# print(product1.popitem())
+
+# print(product1)
+# print(product1.setdefault('title2', 'test'))
+# print(product1)
+
+# print(product1)
+# product1.update({'test': 'value'})
+# product1.update({'price': 200})
+# print(product1)
+
+print(product1.values())
