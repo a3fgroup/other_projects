d8832d0d3c8d2288a4e4b62e8eb99ac5732880ef
a3fgroup <g.koreev@a3f.yaconnect.com>
2020-11-08 20:13:30 +0300

    add lesson 26

---
+++ lesson26.py
@@ -0,0 +1,27 @@
+# def hello(name, word):
+#     print('Hello, ' + name + '. Say ' + word )
+#
+# hello('John', 'hi')
+# hello('Katy', 'hello')
+
+
+# def get_sum(a, b):
+#     print(a + b)
+
+# x = 2
+# y = 5
+# get_sum(1, 3)
+# get_sum(x, y)
+
+
+# def get_sum(a, b):
+#     return a + b
+#
+# print(get_sum(1, 2))
+
+
+def hi():  # В этом примере неявно будет возвращаться None
+    print('Hi')
+
+
+print(hi())
